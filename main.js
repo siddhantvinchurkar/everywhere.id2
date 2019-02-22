7452fc212820ef29b88ae528d5173854337d33e8
Siddhant Vinchurkar <siddhantvinchurkar@gmail.com>
2019-02-22 20:09:35 +0530

    Finished app basics.

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ This is the controlling JS file for xdata-fire.
 ************************************************/
 
 /* Global Variables*/
-var developer = true;
+var developer = true;	// TODO: Change this value to false before pushing to production.
 var loadTimeError = false;
+var theme = true;
 var consoleStyle1 = ['background-color:#222222',
 			'color:#BADA55',
 			'display:inline',
@@ -26,14 +27,34 @@ var consoleStyle3 = ['background-color:#FF6611',
 			'text-align:center',
 			'font-size:1.1em',
 			'font-weight:bold'].join(';');
+var consoleStyle4 = ['background-color:#222222',
+			'color:#FFFF00',
+			'display:inline',
+			'text-shadow:0 1px 0 rgba(0, 0, 0, 0.3)',
+			'text-align:center',
+			'font-size:1.1em',
+			'font-weight:bold'].join(';');
+var consoleStyle5 = ['background-color:#222222',
+			'color:#9999FF',
+			'display:inline',
+			'text-shadow:0 1px 0 rgba(0, 0, 0, 0.3)',
+			'text-align:center',
+			'font-size:1.1em',
+			'font-weight:bold'].join(';');
+var githubUsername = 'unknown';
 var githubUser = 'unknown';
+var githubAccessToken = 'unknown';
 
 window.onload = function(){
+	$('#loading-indicator').fadeIn();
+	$('#loading-indicator').fadeOut();
+	$('#continue-button').fadeOut();
+	$('#skeleton').fadeOut();
 	setTimeout(function(){
 		/* Everything must get executed only after the page has finished loading and there are no errors plus one second. */
 		if(!loadTimeError){
 			/* Register the service worker if it doesn't yet exist. */
-			//if('serviceWorker' in navigator){window.addEventListener('load',()=>{navigator.serviceWorker.register('/sw.js');});}
+			if('serviceWorker' in navigator){window.addEventListener('load',()=>{navigator.serviceWorker.register('/sw.js');});}
 			onAppLoad();
 		}
 	}, 1000);
@@ -53,29 +74,30 @@ window.onerror = function(msg, url, lineNo, columnNo, error){
 
 /* Function Definitions */
 
-function applyDefaultStyles(){
-	/* This function sets the app's styling information to default. */
-	document.getElementById('loading-indicator').innerHTML= 'Hello from Data Fire!<br /><span style="font-size:0.5em;">Sign in with your </span><img id="sign-in-button" height="100px" src="images/logos/github_logo_white.png" style="cursor:pointer;"></img><span style="font-size:0.5em;"> account.</span>';
-	document.getElementById('skeleton').style.display = 'block';
-}
-
-function applyErrorStyles(){
-	/* This function sets the app's styling information to default. */
-	//document.getElementById('body').classList.add('loading-body');
-	document.getElementById('loading-indicator').innerHTML= 'दाल में कुछ काला है । Maybe hit <span style="box-sizing: border-box;border:5px solid #FFFFFF;padding:5px;">F5</span> ?';
-	document.getElementById('loading-indicator').style.display = 'block';
-	document.getElementById('clouds').style.display = 'block';
-	document.getElementById('skeleton').style.display = 'none';
+function applyStyles(styles){
+	/* This function changes the app's styling information. */
+	switch(styles){
+		case 'error':
+			document.getElementById('loading-indicator').innerHTML = 'दाल में कुछ काला है । Maybe hit <span style="box-sizing: border-box;border:5px solid #FFFFFF;padding:5px;">F5</span> ?';
+			$('#loading-indicator').fadeIn();
+			$('#clouds').fadeIn();
+			$('#skeleton').fadeOut();
+			break;
+		default:
+			document.getElementById('loading-indicator').innerHTML = 'Hello from Data Fire!<br /><span style="font-size:0.5em;">Sign in with your </span><img id="sign-in-button" height="100px" src="images/logos/github_logo_white.png" style="cursor:pointer;"></img><span style="font-size:0.5em;"> account.</span>';
+			$('#loading-indicator').fadeIn();
+			break;
+	}
 }
 
 function changeAppState(appState, message){
 	switch(appState){
 		case 'ready':
-			applyDefaultStyles();
+			applyStyles();
 			onAppStateChanged(appState, message);
 			break;
 		case 'error':
-			applyErrorStyles();
+			applyStyles('error');
 			onAppStateChanged(appState, message);
 			break;
 		default: /* What are you trying to do, Sid? */ break;
@@ -83,23 +105,109 @@ function changeAppState(appState, message){
 }
 
 function injectHTML(data, element){
-	/* This function will inject code into the document. */
+	/* This function will inject code (and overwrite any existing code) in the document. */
+	document.getElementById(element).innerHTML = '';
 	document.getElementById(element).innerHTML = data;
 }
 
-function load(url, element){
+function load(url, method, element, returnId){
 	/* This function will asynchronously pull fetch data from the given URL. */
-	document.getElementById(element).innerHTML = '';
+	if(developer) console.log('\n%c ' + method + ' %c '+ url +'\n', consoleStyle4, consoleStyle3);
 	const xhr = new XMLHttpRequest();
-	xhr.responseType = 'text';
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == XMLHttpRequest.DONE){injectHTML(xhr.response, element);}
+	if(element == null) xhr.responseType = 'json';
+	else xhr.responseType = 'text';
+	xhr.onreadystatechange = function(){if(xhr.readyState == XMLHttpRequest.DONE){onLoad(url, element, xhr.response, returnId);}}
+	xhr.open(method, url);
+	try{xhr.send();}catch(error){/* Change app state to errored. */console.log(error);changeAppState('error', error);onLoad(url, element, null, returnId);}
+}
+
+function changeColor(col, amt) {  
+	var usePound = false;
+	if(col[0] == "#"){
+		col = col.slice(1);
+		usePound = true;
 	}
-	xhr.open("GET", url);
-	try{xhr.send();}catch(error){/* Change app state to errored. */changeAppState('error', error);}
+	var num = parseInt(col,16);
+	var r = (num >> 16) + amt;
+	if(r > 255) r = 255;
+	else if(r < 0) r = 0;
+	var b = ((num >> 8) & 0x00FF) + amt;
+	if(b > 255) b = 255;
+	else if(b < 0) b = 0;
+	var g = (num & 0x0000FF) + amt;
+	if(g > 255) g = 255;
+	else if(g < 0) g = 0;
+	return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+function lightenAppBackground(){
+	if(theme){
+	theme = false;
+	var currentColor = '#FF6611';
+	var m=0;
+	var lightenInterval = setInterval(function(){
+		if(m<28){
+			m++;
+			document.getElementById('body').style = 'background-color:' + changeColor(currentColor, (m/2)) + ';';
+			currentColor = changeColor(currentColor, (m/2));
+			if(developer) console.log('\n%c ACTION %c '+ 'Background color changed to ' + currentColor +'\n', consoleStyle5, ['background-color:'+currentColor,'color:#000000','display:inline','text-shadow:0 1px 0 rgba(0, 0, 0, 0.3)','text-align:center','font-size:1.1em','font-weight:bold'].join(';'));
+		}
+		else{
+			clearInterval(lightenInterval);
+			currentColor = '#F6F6F6';
+			document.getElementById('body').style = 'background-color:#F6F6F6;';
+			if(developer) console.log('\n%c ACTION %c '+ 'Background color changed to ' + currentColor +'\n', consoleStyle5, ['background-color:'+currentColor,'color:#000000','display:inline','text-shadow:0 1px 0 rgba(0, 0, 0, 0.3)','text-align:center','font-size:1.1em','font-weight:bold'].join(';'));
+		}
+	}, 100);
+	}
+	else darkenAppBackground();
+}
+
+function darkenAppBackground(){
+	if(!theme){
+	theme = true;
+	var currentColor = '#FFAA66';
+	var n=0;
+	document.getElementById('body').style = 'background-color:#FFAA66;';
+	if(developer) console.log('\n%c ACTION %c '+ 'Background color changed to ' + currentColor +'\n', consoleStyle5, ['background-color:'+currentColor,'color:#FFFFFF','display:inline','text-shadow:0 1px 0 rgba(0, 0, 0, 0.3)','text-align:center','font-size:1.1em','font-weight:bold'].join(';'));
+	var darkenInterval = setInterval(function(){
+		if(n<16){
+			n++;
+			document.getElementById('body').style = 'background-color:' + changeColor(currentColor, -1*(n/3)) + ';';
+			currentColor = changeColor(currentColor, -1*(n/3));
+			if(developer) console.log('\n%c ACTION %c '+ 'Background color changed to ' + currentColor +'\n', consoleStyle5, ['background-color:'+currentColor,'color:#FFFFFF','display:inline','text-shadow:0 1px 0 rgba(0, 0, 0, 0.3)','text-align:center','font-size:1.1em','font-weight:bold'].join(';'));
+		}
+		else{
+			clearInterval(darkenInterval);
+			document.getElementById('body').style = 'background-color:#FF6611;';
+			currentColor = '#FF6611';
+			if(developer) console.log('\n%c ACTION %c '+ 'Background color changed to ' + currentColor +'\n', consoleStyle5, ['background-color:'+currentColor,'color:#FFFFFF','display:inline','text-shadow:0 1px 0 rgba(0, 0, 0, 0.3)','text-align:center','font-size:1.1em','font-weight:bold'].join(';'));
+		}
+	}, 100);
+	}
+	else lightenAppBackground();
 }
 
 /* Listeners */
+
+function onLoad(url, element, data, returnId){
+	if(element !== null) injectHTML(data, element);
+	if(data !== null && developer) console.log('\n%c OK %c ' + 'Fetch complete.' + '\n', consoleStyle1, consoleStyle3);
+	else console.log('\n%c ERROR %c '+ 'Failed to fetch resource at ' + url +'\n', consoleStyle2, consoleStyle3);
+	switch(returnId){
+		case 0: 
+			document.getElementById('loading-indicator').innerHTML = '<span><img id="github-logo" height="100px" src="images/logos/github_logo_white.png"></img><span style="font-size:0.5em;">/&emsp;<img id="github-avatar" height="100px" src="' + githubUser.photoURL + '" style="cursor:pointer; border-radius: 50%;"></img>&emsp;<a href="https://github.com/' + githubUsername + '" target="_blank" style="color:#FFFFFF;">' + githubUsername + '</a></span><br />' + '<span style="font-size:0.4em; line-height:40%; margin-left:20px;">' + data.bio + '</span>';
+			$('#loading-indicator').fadeIn();
+			$('#continue-button').fadeIn();
+			break;
+		case 1:
+			setTimeout(function(){$('#loading-indicator').fadeOut();$('#skeleton').fadeIn();}, 1000);
+			lightenAppBackground();
+			break;
+		default:
+			break;
+	}
+}
 
 function onAppLoad(){
 	/* This callback is triggered when the app has finished loading. */
@@ -107,11 +215,8 @@ function onAppLoad(){
 	/* Change app state to ready. */
 	changeAppState('ready', 'Data Fire is loaded and ready to go!');
 
-	/* Inject some clouds into the root file (index.html) */	
-	load('components/index.clouds.html', 'clouds');
-	
-	/* Inject the skeleton of the app into the root file (index.html) */
-	load('components/index.skeleton.html', 'skeleton');
+	/* Inject some clouds into the root file (index.html) */
+	load('components/index.clouds.html', 'GET', 'clouds');
 	
 	/* Initialize Firebase */
 	firebase.initializeApp({
@@ -126,14 +231,34 @@ function onAppLoad(){
 	/* Data Handlers */
 	
 	/* Handle sign in button click */
-	document.getElementById('sign-in-button').onclick = function(){firebase.auth().signInWithRedirect(new firebase.auth.GithubAuthProvider());}
+	document.getElementById('sign-in-button').onclick = function(){
+		$('#loading-indicator').fadeOut();
+		var provider = new firebase.auth.GithubAuthProvider();
+		/* TODO: Add these scopes when and if you need to use them. */
+		//provider.addScope('user');
+		//provider.addScope('repo');
+		//provider.addScope('gist');
+		firebase.auth().signInWithPopup(provider).then(function(result){onUserSignedIn(result.additionalUserInfo.username, result.user, result.credential.accessToken);}).catch(function(error){console.log(error);});
+	}
 	/* Handle sign in result */
 	firebase.auth().getRedirectResult().then(function(result){if(result.credential)onUserSignedIn(result.user);}).catch(function(error){console.log(error);});
 	/* Handle sign out result */
-	firebase.auth().signOut().then(function(){onUserSignedOut();}).catch(function(error){console.log(error);});}
+	firebase.auth().signOut().then(function(){onUserSignedOut();}).catch(function(error){console.log(error);});
+	
+	/* Handle continue button click */
+	document.getElementById('continue-button').onclick = function(){
+		$('#loading-indicator').fadeOut();
+		setTimeout(function(){document.getElementById('loading-indicator').innerHTML = 'Getting things ready...';$('#loading-indicator').fadeIn();}, 1000);
+		$('#continue-button').fadeOut();
+		$('#clouds').fadeOut();
+		$('#continue-button').fadeOut();
+		/* Inject the page skeleton into the root file (index.html) */
+		load('components/index.skeleton.html', 'GET', 'skeleton', 1);
+	}
+}
 
 function onAppStateChanged(appState, message){
-	/* This callback is triggered when the app changes it's state */
+	/* This callback is triggered when the app changes it's state. */
 	switch(appState){
 		case 'ready':
 			/* Let people know when Data Fire is ready. */
@@ -147,10 +272,13 @@ function onAppStateChanged(appState, message){
 	}
 }
 
-function onUserSignedIn(user){
+function onUserSignedIn(username, user, accessToken){
 	/* This callback is triggered when the user signs in. */
+	githubUsername = username;
 	githubUser = user;
-	console.log(user + '\n\nSigned in!');
+	githubAccessToken = accessToken;
+	console.log('\n%c OK %c ' + 'Signed in as ' + githubUser.displayName + '!\n', consoleStyle1, consoleStyle3);
+	load('https://api.github.com/users/' + githubUsername, 'GET', null, 0);
 }
 
 function onUserSignedOut(){
